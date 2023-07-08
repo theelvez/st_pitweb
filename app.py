c7bb8c8ca30d9ef18f1e72a5f9550605bbfbce52
= <jonmorri@msn.com>
2023-07-08 08:04:36 -0700

    add db stuff

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import os
+from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=pitwebstorage;AccountKey=mr4ZIFYYqhct+TvyhAR/J0JBEzvoaNlFRBOMttoxLzOaQG2Bmyv1w/6YN/ui0V0NTC+B/OeMWzvs+AStPwOBEA==;BlobEndpoint=https://pitwebstorage.blob.core.windows.net/;FileEndpoint=https://pitwebstorage.file.core.windows.net/;QueueEndpoint=https://pitwebstorage.queue.core.windows.net/;TableEndpoint=https://pitwebstorage.table.core.windows.net/"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///race_results.db'
 db = SQLAlchemy(app)
+
+# Azure Blob Storage configuration
+BLOB_STORAGE_CONNECTION_STRING = '<your_blob_storage_connection_string>'
+BLOB_CONTAINER_NAME = '<your_blob_container_name>'
+BLOB_DATABASE_FILE_NAME = 'race_results.db'
 
 class RaceResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,14 +21,60 @@ class RaceResult(db.Model):
     def __repr__(self):
         return f'<RaceResult {self.name}>'
 
+def initialize_blob_storage():
+    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
+    
+    # Check if the container exists, create it if it doesn't
+    if not container_client.exists():
+        container_client.create_container()
+
+def download_database_file():
+    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
+    
+    # Download the database file from Blob Storage
+    with open('race_results.db', 'wb') as file:
+        blob_client = container_client.get_blob_client(BLOB_DATABASE_FILE_NAME)
+        download_stream = blob_client.download_blob()
+        file.write(download_stream.readall())
+
+def upload_database_file():
+    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
+    
+    # Upload the database file to Blob Storage
+    with open('race_results.db', 'rb') as file:
+        blob_client = container_client.get_blob_client(BLOB_DATABASE_FILE_NAME)
+        blob_client.upload_blob(file)
+
+
+
+@app.before_first_request
+def before_first_request():
+    initialize_blob_storage()
+    download_database_file()
+
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception=None):
+    upload_database_file()
+
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
 
 @app.route('/results')
 def results():
     data = RaceResult.query.all()  # Get all race results from the database
     return render_template('results.html', data=data)
+
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -53,17 +103,18 @@ def upload_auto():
     db.session.commit()
     return jsonify({'message': 'Data received'}), 200
 
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create the database table
     app.run()
-
-
--- blobstor.ps1
+++ blobstor.ps1
@@ -1,12 +1,8 @@
-# Set variables
-storageAccountName="pitwebStorage"
-storageContainerName="<pitwebStorageContainer>"
+$storageAccountName = "pitwebstorage"
+$storageContainerName = "pitwebstoragecontainer"
 
-# Create a storage account
-az storage account create --name $storageAccountName --resource-group <resource_group_name> --location <location> --sku Standard_LRS
+az storage account create --name $storageAccountName --resource-group "pitwebResources" --location "westus" --sku Standard_LRS
 
-# Retrieve the storage account connection string
-connectionString=$(az storage account show-connection-string --name $storageAccountName --resource-group <resource_group_name> --query connectionString --output tsv)
+$connectionString = az storage account show-connection-string --name $storageAccountName --resource-group pitwebResources --query connectionString --output tsv
 
-# Create a storage container
 az storage container create --name $storageContainerName --connection-string $connectionString
