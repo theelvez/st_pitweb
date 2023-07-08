77a4a925925eb7757850fa6128192c18c702ddd5
theelvez <80179310+theelvez@users.noreply.github.com>
2023-07-08 10:44:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from azure.storage.blob import BlobServiceClient
 
 app = Flask(__name__)
 
-# Azure Blob Storage configuration
-BLOB_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=pitwebstorage;AccountKey=mr4ZIFYYqhct+TvyhAR/J0JBEzvoaNlFRBOMttoxLzOaQG2Bmyv1w/6YN/ui0V0NTC+B/OeMWzvs+AStPwOBEA==;EndpointSuffix=core.windows.net"
-BLOB_CONTAINER_NAME = "pitwebstoragecontainer"
-BLOB_DATABASE_FILE_NAME = "race_results.db"
+# MySQL database configuration
+DB_HOST = "pitwebdb.mysql.database.azure.com"
+DB_USER = "pwadmin"
+DB_PASSWORD = "sp33dtrack3R1!"
+DB_NAME = "race_results"
 
-# Construct the blob storage URI
-blob_uri = f"azureblob://{BLOB_STORAGE_CONNECTION_STRING}/{BLOB_CONTAINER_NAME}/{BLOB_DATABASE_FILE_NAME}"
+# Create the MySQL connection URL without SSL parameters
+DB_CONNECTION_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 # Set the SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_DATABASE_URI"] = blob_uri
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
@@ -26,39 +27,6 @@ class RaceResult(db.Model):
 
     def __repr__(self):
         return f"<RaceResult {self.name}>"
-
-def initialize_blob_storage():
-    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
-    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
-
-    # Check if the container exists, create it if it doesn't
-    if not container_client.exists():
-        container_client.create_container()
-
-def download_database_file():
-    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
-    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
-
-    # Download the database file from Blob Storage
-    with open(BLOB_DATABASE_FILE_NAME, "wb") as file:
-        blob_client = container_client.get_blob_client(BLOB_DATABASE_FILE_NAME)
-        download_stream = blob_client.download_blob()
-        file.write(download_stream.readall())
-
-def upload_database_file():
-    blob_service_client = BlobServiceClient.from_connection_string(BLOB_STORAGE_CONNECTION_STRING)
-    container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
-
-    # Upload the database file to Blob Storage
-    with open(BLOB_DATABASE_FILE_NAME, "rb") as file:
-        blob_client = container_client.get_blob_client(BLOB_DATABASE_FILE_NAME)
-        blob_client.upload_blob(file)
-
-# Initialize blob storage
-initialize_blob_storage()
-
-# Download the database file from Blob Storage
-download_database_file()
 
 @app.route("/")
 def home():
