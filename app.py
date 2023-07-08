2856b081e45934ad7217943e9208c801a83f63fd
theelvez <80179310+theelvez@users.noreply.github.com>
2023-07-08 01:53:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///race_results.db'
+db = SQLAlchemy(app)
+
+class RaceResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    car = db.Column(db.String(120), nullable=False)
+    run_number = db.Column(db.Integer, nullable=False)
+    top_speed = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<RaceResult {self.name}>'
 
 @app.route('/')
 def home():
@@ -8,17 +21,25 @@ def home():
 
 @app.route('/results')
 def results():
-    # For now, we'll just pass some dummy data to the template.
-    # Later, you can replace this with real data from your database.
-    data = [
-        {'name': 'John Doe', 'car': 'Ford Mustang 2020', 'run_number': 1, 'top_speed': 120},
-        {'name': 'Jane Doe', 'car': 'Chevrolet Camaro 2021', 'run_number': 2, 'top_speed': 115},
-    ]
+    data = RaceResult.query.all()  # Get all race results from the database
     return render_template('results.html', data=data)
 
-@app.route('/upload')
+@app.route('/upload', methods=['POST'])
 def upload():
-    return render_template('upload.html')
+    # Here, you would typically get the data from the form and save it to the database.
+    # For now, we'll just return a placeholder message.
+    return jsonify({'message': 'Data received'}), 200
+
+@app.route('/upload_auto', methods=['POST'])
+def upload_auto():
+    data = request.get_json()  # Get JSON data sent in the request
+    if not data:
+        return jsonify({'message': 'No data provided'}), 400
+    # Save the data to the database
+    result = RaceResult(name=data['name'], car=data['car'], run_number=data['run_number'], top_speed=data['top_speed'])
+    db.session.add(result)
+    db.session.commit()
+    return jsonify({'message': 'Data received'}), 200
 
 @app.route('/about')
 def about():
@@ -28,16 +49,6 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route('/upload_auto', methods=['POST'])
-def upload_auto():
-    data = request.get_json()  # Get JSON data sent in the request
-    if not data:
-        return jsonify({'message': 'No data provided'}), 400
-    # Here, you would typically save the data to your database.
-    # For now, we'll just print it to the console.
-    print(data)
-    return jsonify({'message': 'Data received'}), 200
-
-
 if __name__ == '__main__':
+    db.create_all()  # Create the database table
     app.run(debug=True)
