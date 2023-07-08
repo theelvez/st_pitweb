2a68739e0ad06a4f8932c762df1518ffa7477d19
Jonathan Morrison <jonmorri@msn.com>
2023-07-08 06:17:56 -0700

    missing imports

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'instance', 'race_results.db')
