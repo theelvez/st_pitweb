d83bfe010c4e2f52f348c5a095594cff50abbe9a
theelvez <80179310+theelvez@users.noreply.github.com>
2023-07-08 01:59:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def contact():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database table
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=True)
+
