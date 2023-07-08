472d4582f79823bbf7bc5f5f4972ce4f8585ba52
= <jonmorri@msn.com>
2023-07-08 11:10:25 -0700

    updates

--- blobstor.ps1
+++
@@ -1,8 +0,0 @@
-$storageAccountName = "pitwebstorage"
-$storageContainerName = "pitwebstoragecontainer"
-
-az storage account create --name $storageAccountName --resource-group "pitwebResources" --location "westus" --sku Standard_LRS
-
-$connectionString = az storage account show-connection-string --name $storageAccountName --resource-group pitwebResources --query connectionString --output tsv
-
-az storage container create --name $storageContainerName --connection-string $connectionString
