d161cacea4c1961c1df5070a6e5f7cadc8113663
Jonathan Morrison <jonmorri@msn.com>
2023-07-09 13:33:26 -0700

    test tools

--- tests/postsim.py
+++ tests/postsim.py
@@ -7,21 +7,25 @@ import time
 def format_data_to_json(row):
     # Format the CSV row into a JSON object
     data = {
-        'Name': row[0],
-        'Car': row[1],
-        'Run Number': int(row[2]),
-        'Top Speed': float(row[3])
+        'name': row[0],
+        'car': row[1],
+        'run_number': int(row[2]),
+        'top_speed': float(row[3])
     }
     return json.dumps(data)
 
 
 def make_post_request(json_data):
     # Make an HTTP POST request to the server
-    url = 'https://pitweb.azurewebsites.net/upload'  # Replace with the actual URL
+    url = 'https://pitweb.azurewebsites.net/upload_auto'  # Replace with the actual URL
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, data=json_data, headers=headers)
+    
     if response.status_code == 200:
         print('POST request successful')
+    elif response.status_code == 400:
+        error_message = response.json().get('error')  # Assuming the server returns the error message as JSON
+        print(f'POST request failed. Status Code: 400. Error Message: {error_message}')
     else:
         print(f'POST request failed. Status Code: {response.status_code}')
 
@@ -39,5 +43,5 @@ def process_csv_file(file_path, wait_time):
 
 if __name__ == '__main__':
     file_path = 'dummydata.csv'
-    seconds = 30 # Replace with the desired number of seconds to wait
+    seconds = 5  # Replace with the desired number of seconds to wait
     process_csv_file(file_path, seconds)
