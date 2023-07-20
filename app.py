8bcdcf3b2398c1fde20e1dc22dfce1cc5ac71413
theelvez <80179310+theelvez@users.noreply.github.com>
2023-07-19 22:30:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,165 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc, distinct
-import random
-
 
 app = Flask(__name__)
 
-# MySQL database configuration
-DB_HOST = "pitwebdb.mysql.database.azure.com"
-DB_USER = "pwadmin"
-DB_PASSWORD = "sp33dtrack3R1!"
-DB_NAME = "race_results"
+# SQLite database configuration
+DB_NAME = "race_data.db"  # Replace with your SQLite database file name
 
-# Create the MySQL connection URL without SSL parameters
-DB_CONNECTION_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+# Create the SQLite connection URL
+DB_CONNECTION_URL = f"sqlite:///{DB_NAME}"
 
 # Set the SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+
 class RaceResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(80), nullable=False)
     car = db.Column(db.String(120), nullable=False)
-    run_number = db.Column(db.Integer, nullable=False)
+    run = db.Column(db.Integer, nullable=False)
     top_speed = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return f"<RaceResult {self.name}>"
 
+
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    device_id = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    car = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Driver {self.name}>"
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/results")
 def results():
-      
-    top_speed = RaceResult.query.order_by(desc("top_speed"))
-    run_number = RaceResult.query.order_by("run_number", desc("top_speed"))
-    name = RaceResult.query.order_by("name", "run_number")
-    leader = RaceResult.query.order_by(desc("top_speed")).first()
-    last = RaceResult.query.order_by(desc("id")).limit(5)
-    club = RaceResult.query.filter(RaceResult.top_speed > 200).order_by(RaceResult.name).with_entities(RaceResult.name).distinct()
-    
-    data = RaceResult.query.order_by(desc("top_speed"), "run_number", "name")
-    
-    return render_template("results.html", top_speed=top_speed, run_number=run_number, name=name, leader=leader, club=club, last=last)
-    
+    top_speed = (
+        RaceResult.query.order_by(desc(RaceResult.top_speed))
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .all()
+    )
+    run_number = (
+        RaceResult.query.order_by(RaceResult.run, desc(RaceResult.top_speed))
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .all()
+    )
+    name = (
+        RaceResult.query.order_by(RaceResult.name, RaceResult.run)
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .all()
+    )
+    leader = (
+        RaceResult.query.order_by(desc(RaceResult.top_speed))
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .first()
+    )
+    last = (
+        RaceResult.query.order_by(desc(RaceResult.id))
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .limit(5)
+        .all()
+    )
+    club = (
+        RaceResult.query.filter(RaceResult.top_speed > 200)
+        .order_by(RaceResult.name)
+        .with_entities(RaceResult.name)
+        .distinct()
+        .all()
+    )
+
+    data = (
+        RaceResult.query.order_by(desc(RaceResult.top_speed), RaceResult.run, RaceResult.name)
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .all()
+    )
+
+    return render_template(
+        "results.html",
+        top_speed=top_speed,
+        run_number=run_number,
+        name=name,
+        leader=leader,
+        club=club,
+        last=last,
+    )
+
+
 @app.route("/onedriver")
 def onedriver():
-      
-    driver = RaceResult.query.filter(RaceResult.name == request.args.get('name'))
-    top_speed = RaceResult.query.filter(RaceResult.name == request.args.get('name')).order_by(desc("top_speed")).limit(1)
-    
-    return render_template("driver.html", name=request.args.get('name'), driver=driver, top_speed=top_speed)
-        
+    driver_name = request.args.get("name")
+    driver = (
+        Drivers.query.filter(Drivers.name == driver_name)
+        .with_entities(Drivers.device_id, Drivers.name, Drivers.car)
+        .first()
+    )
+    top_speed = (
+        RaceResult.query.filter(RaceResult.name == driver_name)
+        .order_by(desc(RaceResult.top_speed))
+        .with_entities(
+            RaceResult.device_id,
+            RaceResult.name,
+            RaceResult.car,
+            RaceResult.run,
+            RaceResult.top_speed,
+        )
+        .limit(1)
+        .first()
+    )
+
+    return render_template(
+        "driver.html", name=driver_name, driver=driver, top_speed=top_speed
+    )
+
 
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
@@ -65,11 +168,26 @@ def upload():
         if not data:
             return jsonify({"message": "No data provided"}), 400
         # Save the data to the database
+        device_id = data.get("device_id")
+        name = data.get("name")
+        car = data.get("car")
+        run_number = int(data.get("run_number"))
+        top_speed = float(data.get("top_speed"))
+
+        # Check if the driver already exists in the "drivers" table
+        driver = Drivers.query.filter(Drivers.device_id == device_id).first()
+        if not driver:
+            # Create a new driver if not found
+            driver = Driver(device_id=device_id, name=name, car=car)
+            db.session.add(driver)
+
+        # Save the race result
         result = RaceResult(
-            name=data["name"],
-            car=data["car"],
-            run_number=int(data["run_number"]),
-            top_speed=float(data["top_speed"]),
+            device_id=device_id,
+            name=name,
+            car=car,
+            run=run_number,
+            top_speed=top_speed,
         )
         db.session.add(result)
         db.session.commit()
@@ -77,33 +195,59 @@ def upload():
     else:
         return render_template("upload.html")
 
+
 @app.route("/upload_auto", methods=["POST"])
 def upload_auto():
     data = request.get_json()  # Get JSON data sent in the request
     if not data:
         return jsonify({"message": "No data provided"}), 400
     # Save the data to the database
+    device_id = data.get("device_id")
+    name = data.get("name")
+    car = data.get("car")
+    run_number = data.get("run_number")
+    top_speed = data.get("top_speed")
+
+    # Check if the driver already exists in the "drivers" table
+    driver = Drivers.query.filter(Drivers.device_id == device_id).first()
+    if not driver:
+        # Create a new driver if not found
+        driver = Driver(device_id=device_id, name=name, car=car)
+        db.session.add(driver)
+
+    # Save the race result
     result = RaceResult(
-        name=data["name"],
-        car=data["car"],
-        run_number=data["run_number"],
-        top_speed=data["top_speed"],
+        device_id=device_id,
+        name=name,
+        car=car,
+        run=run_number,
+        top_speed=top_speed,
     )
     db.session.add(result)
     db.session.commit()
     return jsonify({"message": "Data received"}), 200
 
-@app.route("/emptydb")
+
+@app.route("/emptydb", methods=["GET", "POST"])
 def emptydb():
-    return render_template("emptydb.html")
+    if request.method == "GET":
+        return render_template("emptydb.html")
+    elif request.method == "POST":
+        # Delete all records from the database
+        RaceResult.query.delete()
+        db.session.commit()
+        return jsonify({"message": "Database emptied"}), 200
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
